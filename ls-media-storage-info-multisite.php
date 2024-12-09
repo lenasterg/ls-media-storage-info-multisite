c8c18e4722e9ab38db06ee86788db723e8fcf8ac
Lena Stergatou <lenasterg@users.noreply.github.com>
2024-12-09 12:13:42 +0200

    Update ls-media-storage-info-multisite.php

--- ls-media-storage-info-multisite.php
+++ ls-media-storage-info-multisite.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: LS Media Storage Info for Multisite
-Description: Displays used and available storage space in the Media Library for WordPress Multisite.
+Description: Displays used and available storage space  in the Media Library of each subsite for WordPress Multisite.
 Version: 1.0
 Author: lenasterg
 License: GPLv2 or later
@@ -17,10 +17,10 @@ if ( ! defined( 'ABSPATH' ) ) {
  *
  * @return void
  */
-function display_notice_storage_info_multisite() {
+function ls_display_notice_storage_info_multisite() {
 
 	echo '<div class="notice notice-info is-dismissible">';
-	display_storage_info_multisite();
+	ls_display_storage_info_multisite();
 	echo '</div>';
 }
 
@@ -29,7 +29,7 @@ function display_notice_storage_info_multisite() {
  *
  * @return void
  */
-function display_storage_info_multisite() {
+function ls_display_storage_info_multisite() {
 	// Get used and available space
 	$quota = get_space_allowed();
 	$used  = get_space_used();
@@ -43,18 +43,18 @@ function display_storage_info_multisite() {
 
 	// Display the storage information with localization support
 
-	echo '<p><strong>' . esc_html__( 'Storage Space' ) . ':</strong> ';
+	echo '<p><strong>' . esc_html__( 'Storage Space','ls-media-storage-info-multisite' ) . ':</strong> ';
 
 	$quota_t = sprintf(
 				/* translators: %s: Number of megabytes. */
-		__( '%s MB' ),
+		__( '%s MB','ls-media-storage-info-multisite' ),
 		number_format_i18n( $quota )
 	);
 	echo  esc_html($quota_t . ' / ');
 	/* translators: 1: Number of megabytes, 2: Percentage. */
 	$text = sprintf(
 				/* translators: 1: Number of megabytes, 2: Percentage. */
-		__( '%1$s MB (%2$s%%) Space Used' ),
+		__( '%1$s MB (%2$s%%) Space Used','ls-media-storage-info-multisite' ),
 		number_format_i18n( $used, 2 ),
 		$percentused
 	);
@@ -63,24 +63,29 @@ function display_storage_info_multisite() {
 	echo '</p>';
 }
 
+add_action( 'init', 'ls_media_storage_info_multisite_load_textdomain' );
+
+function ls_media_storage_info_multisite_load_textdomain() {
+	load_plugin_textdomain( 'ls-media-storage-info-multisite', false, dirname( plugin_basename( __FILE__ ) ) . '/languages' ); 
+}
+
 /**
  * Displays the storage info on the Media Library page and on the 'upload media' window.
  *
  * @param string $hook_suffix The page hook name
  * @return void
  */
-function show_storage_info_on_media_page( $hook_suffix ) {
+function ls_show_storage_info_on_media_page( $hook_suffix ) {
 	// Check if it's a multisite
 	if ( ! is_multisite() || ! current_user_can( 'upload_files' ) || get_site_option( 'upload_space_check_disabled' )
 	) {
 		return true;
 	} else {
 		if ( ( 'media-new.php' === $hook_suffix ) || ( 'upload.php' === $hook_suffix ) ) { // Media Library page
-			add_action( 'admin_notices', 'display_notice_storage_info_multisite' );
+			add_action( 'admin_notices', 'ls_display_notice_storage_info_multisite' );
 		} else {
-			add_action( 'post-upload-ui', 'display_storage_info_multisite' );
+			add_action( 'post-upload-ui', 'ls_display_storage_info_multisite' );
 		}
 	}
 }
-add_action( 'admin_enqueue_scripts', 'show_storage_info_on_media_page' );
-
+add_action( 'admin_enqueue_scripts', 'ls_show_storage_info_on_media_page' );
